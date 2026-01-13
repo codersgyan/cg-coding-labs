61668f43b4122588b18993d8488e6bf728a7c031
Ajay <ajayxa1@gmail.com>
2026-01-13 15:29:03 +0530

    remove gap

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,7 +1,7 @@
 function Header() {
   return (
   
-      <h1>Welcome to Coder&apos;s Gyan </h1>
+      <h1>Welcome to Coder&apos;s Gyan</h1>
       <p>Sharpen your fundamentals. Build with clarity.</p>
 
   );
