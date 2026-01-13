58b8c2ba2808f1b8e39261775f749b27af78b6dd
Ajay <ajayxa1@gmail.com>
2026-01-13 14:11:14 +0530

    add bugbounty Q1 jsx rule

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+function Header() {
+  return (
+  
+      <h1>Welcome to Coder&apos;s Gyan </h1>
+      <p>Sharpen your fundamentals. Build with clarity.</p>
+   
+  );
+}
+
+export default Header;
