1ff51361c56b9da23cae7c9508b3d78709ab1e39
Ajay <ajayxa1@gmail.com>
2026-01-19 12:55:28 +0530

    add quesiton custom hooks

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import "./App.css";
+
+const App = () => {
+  return (
+    <div className="container">
+      <div className="card">
+        <h1 className="title">Coder's Gyan</h1>
+        <p className="subtitle">You can do this. Good luck</p>
+
+        <div className="status-tag">
+          <code>system_status: active</code>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default App;
