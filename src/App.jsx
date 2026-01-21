de8ccab2f80a1d2486a6aa1d584bddc68159cc69
Ajay <ajayxa1@gmail.com>
2026-01-22 04:18:26 +0530

    update main.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,16 @@
-import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 import Dashboard from "./Dashboard";
 import Login from "./Login";
 const App = () => {
   const isAuth = false;
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="/login" element={<Login />} />
-      </Routes>
-    </BrowserRouter>
+    <Routes>
+      <Route
+        path="/dashboard"
+        element={isAuth ? <Dashboard /> : <Navigate to="/login" />}
+      />
+      <Route path="/login" element={<Login />} />
+    </Routes>
   );
 };
 
