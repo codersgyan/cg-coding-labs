237171bd1b1ad46758ea537c8d87c72b2f88e484
Ajay <ajayxa1@gmail.com>
2026-02-02 13:49:43 +0530

    update UI

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,19 @@
 import React from "react";
 import "./App.css";
-
+const labData = {
+  title: "Coder's Gyan",
+  welcomeMsg: "Your environment is ready. Let's solve this challenge!",
+  status: "ready_to_compile",
+};
 const App = () => {
   return (
     <div className="container">
       <div className="card">
-        <h1 className="title">Coder's Gyan</h1>
-        <p className="subtitle">You can do this. Good luck</p>
+        <h1 className="title">{labData.title}</h1>
+        <p className="subtitle">{labData.welcomeMsg}</p>
 
         <div className="status-tag">
-          <code>system_status: active</code>
+          <code>system_status: {labData.status}</code>
         </div>
       </div>
     </div>
